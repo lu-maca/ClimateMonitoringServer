cc5fd426261a22a847bc0ed13c06c21074af52b1
Luca <luca.macavero@dorbit.space>
2024-07-28 18:15:19 +0200

    implement main of server

--- src/uni/climatemonitor/server/ServerMain.java
+++ src/uni/climatemonitor/server/ServerMain.java
@@ -1,11 +1,19 @@
 package uni.climatemonitor.server;
 
+import uni.climatemonitor.common.IDatabaseService;
+
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
 
 public class ServerMain {
 
-
-    public static void main(String args[]) {
-
-
+    public static void main(String args[]) throws RemoteException {
+        DatabaseServiceImpl dbService = new DatabaseServiceImpl();
+        IDatabaseService ids = (IDatabaseService) UnicastRemoteObject.exportObject(dbService, 8080);
+        Registry registry = LocateRegistry.createRegistry(1099);
+        registry.rebind("dbService", dbService);
+        System.out.println("Server ready");
     }
 }
